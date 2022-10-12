7aa5d3cc5132901f80a682d2ffec13bcef38013e
Vecktor <v3ckt0r.tg@gmail.com>
2022-10-13 00:18:03 +0300

    added swagger to observer

--- routes/behavioral/observer.js
+++ routes/behavioral/observer.js
@@ -22,16 +22,102 @@ export class ObserverRoute extends BaseRoute {
         return router
     }
 
+    /**
+     * @swagger
+     * /behavioral/observer/notifiers:
+     *   get:
+     *     summary: Retrieve available notifiers
+     *     tags: [Behavioral]
+     *     responses:
+     *       200:
+     *         description: Available notifiers
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 status:
+     *                   type: string
+     *                   default: ok
+     *                 data:
+     *                   type: array
+     *                   items:
+     *                     type: string
+     *                 error:
+     *                   type: string
+     *                   nullable: true
+     *                   default: null
+     */
     getAllNotifiers(req, res) {
         const list = this.service.getAllNotifiers()
         this.sendOk(res, list)
     }
 
+    /**
+     * @swagger
+     * /behavioral/observer/notifiers/current:
+     *   get:
+     *     summary: Retrieve current notifiers
+     *     tags: [Behavioral]
+     *     responses:
+     *       200:
+     *         description: Current notifiers
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 status:
+     *                   type: string
+     *                   default: ok
+     *                 data:
+     *                   type: array
+     *                   items:
+     *                     type: string
+     *                 error:
+     *                   type: string
+     *                   nullable: true
+     *                   default: null
+     */
     getCurrentNotifiers(req, res) {
         const list = this.service.getCurrentNotifiers()
         this.sendOk(res, list)
     }
 
+    /**
+     * @swagger
+     * /behavioral/observer/notifiers/{name}:
+     *   post:
+     *     summary: Add notifier
+     *     tags: [Behavioral]
+     *     parameters:
+     *       - name: name
+     *         in: path
+     *         description: Notifier's name
+     *         required: true
+     *         schema:
+     *           type: string
+     *     responses:
+     *       200:
+     *         description: Success
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 status:
+     *                   type: string
+     *                   default: ok
+     *                 data:
+     *                   type: boolean
+     *                   default: true
+     *                 error:
+     *                   type: string
+     *                   nullable: true
+     *                   default: null
+     *       422:
+     *         $ref: '#/components/responses/422ValidationError'
+     */
     addNotifier(req, res) {
         const name = req.params.name
 
@@ -43,6 +129,40 @@ export class ObserverRoute extends BaseRoute {
         }
     }
 
+    /**
+     * @swagger
+     * /behavioral/observer/notifiers/{name}:
+     *   delete:
+     *     summary: Remove notifier
+     *     tags: [Behavioral]
+     *     parameters:
+     *       - name: name
+     *         in: path
+     *         description: Notifier's name
+     *         required: true
+     *         schema:
+     *           type: string
+     *     responses:
+     *       200:
+     *         description: Success
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 status:
+     *                   type: string
+     *                   default: ok
+     *                 data:
+     *                   type: boolean
+     *                   default: true
+     *                 error:
+     *                   type: string
+     *                   nullable: true
+     *                   default: null
+     *       422:
+     *         $ref: '#/components/responses/422ValidationError'
+     */
     removeNotifier(req, res) {
         const name = req.params.name
 
@@ -54,11 +174,71 @@ export class ObserverRoute extends BaseRoute {
         }
     }
 
+    /**
+     * @swagger
+     * /behavioral/observer/notifications:
+     *   get:
+     *     summary: Retrieve sent notifications
+     *     tags: [Behavioral]
+     *     responses:
+     *       200:
+     *         description: Sent notifications
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 status:
+     *                   type: string
+     *                   default: ok
+     *                 data:
+     *                   type: array
+     *                   items:
+     *                     type: string
+     *                 error:
+     *                   type: string
+     *                   nullable: true
+     *                   default: null
+     */
     getNotifications(req, res) {
         const notifications = this.service.getNotifications()
         this.sendOk(res, notifications)
     }
 
+    /**
+     * @swagger
+     * /behavioral/observer/notifications/{message}:
+     *   post:
+     *     summary: Send notification
+     *     tags: [Behavioral]
+     *     parameters:
+     *       - name: message
+     *         in: path
+     *         description: Notification's message
+     *         required: true
+     *         schema:
+     *           type: string
+     *     responses:
+     *       200:
+     *         description: Success
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 status:
+     *                   type: string
+     *                   default: ok
+     *                 data:
+     *                   type: boolean
+     *                   default: true
+     *                 error:
+     *                   type: string
+     *                   nullable: true
+     *                   default: null
+     *       422:
+     *         $ref: '#/components/responses/422ValidationError'
+     */
     notify(req, res) {
         const message = req.params.message
         if (!message) {
