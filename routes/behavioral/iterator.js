74b56a18705593390e87ebb57a56790fc9fdb23c
Vecktor <v3ckt0r.tg@gmail.com>
2022-10-13 00:24:35 +0300

    added swagger to iterator

--- routes/behavioral/iterator.js
+++ routes/behavioral/iterator.js
@@ -19,16 +19,107 @@ export class IteratorRoute extends BaseRoute {
         return router
     }
 
+    /**
+     * @swagger
+     * /behavioral/iterator/list:
+     *   get:
+     *     summary: Retrieve list
+     *     tags: [Behavioral]
+     *     responses:
+     *       200:
+     *         description: List
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 status:
+     *                   type: string
+     *                   default: ok
+     *                 data:
+     *                   type: array
+     *                   items:
+     *                     type: string
+     *                 error:
+     *                   type: string
+     *                   nullable: true
+     *                   default: null
+     */
     getList(req, res) {
         const list = this.service.getList()
         this.sendOk(res, list)
     }
 
+    /**
+     * @swagger
+     * /behavioral/iterator/reversed-list:
+     *   get:
+     *     summary: Retrieve list
+     *     tags: [Behavioral]
+     *     responses:
+     *       200:
+     *         description: Reversed list
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 status:
+     *                   type: string
+     *                   default: ok
+     *                 data:
+     *                   type: array
+     *                   items:
+     *                     type: string
+     *                 error:
+     *                   type: string
+     *                   nullable: true
+     *                   default: null
+     */
     getReversedList(req, res) {
         const list = this.service.getReversedList()
         this.sendOk(res, list)
     }
 
+    /**
+     * @swagger
+     * /behavioral/iterator/add:
+     *   post:
+     *     summary: Add new value to list
+     *     tags: [Behavioral]
+     *     requestBody:
+     *       description: Value's info
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             type: object
+     *             properties:
+     *               value:
+     *                 description: Value
+     *                 type: string
+     *                 required: true
+     *     responses:
+     *       200:
+     *         description: Success
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 status:
+     *                   type: string
+     *                   default: ok
+     *                 data:
+     *                   type: boolean
+     *                   default: true
+     *                 error:
+     *                   type: string
+     *                   nullable: true
+     *                   default: null
+     *       422:
+     *         $ref: '#/components/responses/422ValidationError'
+     */
     add(req, res) {
         const value = req.body.value?.toString()
 
